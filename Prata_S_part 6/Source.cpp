0b81c1107bbf5debe4885220cb71c61363925f92
Ca5tor <48414173+Ca5tor@users.noreply.github.com>
2021-07-11 03:02:47 +0300

    Начало выполнения 4го задания

--- Prata_S_part 6/Source.cpp
+++ Prata_S_part 6/Source.cpp
@@ -95,18 +95,71 @@ void Ex3() {
 	bool flag = true;							// Что-то вроде флага для while
 
 	while (flag && std::cin >> var) {			// Сидим в цикле пока v истинно
-		for (int i = 0; i < SIZE; i++) {		// проходим по всему массиву и
+		for (int i = 0; i < SIZE; i++) 		// проходим по всему массиву и
 			if (var == choice[i]) {				// если введенный символ совпадёт с символом из массива choice,
 				std::cout << str + out_str[i];	// выводим строку ответа
 				flag = false;					// Откл while
 				break;							// выходим из for т.к. символ найден
 			}
-		}
+		
 
 		if (flag) std::cout << "Please enter a c, p, t or g: ";	// Просим юзера о вводе из предложенных символов
 		
 	}
 		
+}
+
+void Ex4() {
+/*
+Когда вы вступите в Благотворительный Орден Программистов (БОП), к вам
+могут обращаться на заседаниях БОП по вашему реальному имени, по
+должности либо секретному имени БОП. Напишите программу, которая может
+выводить списки членов по реальным именам, должностям, секретным именам либо
+по предпочтению самого члена. В основу положите следующую структуру:
+
+// Структура имен Благотворительного Ордена Программистов (БОП)
+struct bop{
+	char fullname[strsize]; // реальное имя
+	char title[strsize]; // должность
+	char bopname[strsize]; // секретное имя БОП
+	int preference; // 0 = полное имя, 1 = титул, 2 = имя БОП
+};
+
+В этой программе создайте небольшой массив таких структур и
+инициализируйте его соответствующими значениями. Пусть программа запустит цикл,
+который даст возможность пользователю выбирать разные альтернативы:
+
+a. display by name b. display by title
+c. display by bopname d. display by preference
+q. quit
+
+Обратите внимание, что "display by preference" (отображать по предпочтениям)
+не означает, что нужно отобразить член preference; это значит, что
+необходимо отобразить член структуры, который соответствует значению preference.
+Например, если preference равно 1, то выбор d должен вызвать отображение
+должности данного программиста. Пример запуска этой программы может
+выглядеть следующим образом:
+
+Benevolent Order of Programmers Report
+a. display by name b. display by title
+c. display by bopname d. display by preference
+q. quit
+Enter your choice: a
+Wimp Macho
+Raki Rhodes
+Celia Laiter
+Hoppy Hipman
+Pat Hand
+Next choice: d
+Wimp Macho
+Junior Programmer
+MIPS
+Analyst Trainee
+LOOPY
+Next choice: q
+Bye!
+*/
+
 }
 
 void main() {
